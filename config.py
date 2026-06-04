259ec04cf31268270fbdcbca82bfd8d20c052bfe
Hitesh Gourana <hiteshgourana2004@gmail.com>
2026-06-04 09:55:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = "your_secret_key"
+
+    MYSQL_HOST = ""
+    MYSQL_USER = ""
+    MYSQL_PASSWORD = ""
+    MYSQL_DB = ""
+
+    # AWS S3
+    AWS_ACCESS_KEY_ID = ""
+    AWS_SECRET_ACCESS_KEY = ""
+    AWS_BUCKET_NAME = ""
+    AWS_REGION = ""
